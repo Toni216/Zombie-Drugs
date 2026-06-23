b6306a1eb759c94eb69472eccc2c22b92665d869
Toni <rmantoniomanuel.dev@proton.me>
2026-06-23 18:50:31 +0200

    feat: setup inicial del proyecto y ZombieDrugs

---
+++ src/main/java/com/cipollomods/zombiedrugs/ZombieDrugs.java
@@ -0,0 +1,13 @@
+package com.cipollomods.zombiedrugs;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(ZombieDrugs.MOD_ID)
+public class ZombieDrugs {
+    public static final String MOD_ID = "zombiedrugs";
+
+    public ZombieDrugs() {
+
+    }
+}
